16a73ce191c6a169bff3a0f68e3a348757e64f08
Pitsco <derrickhuang2@gmail.com>
2023-09-30 11:04:49 -0700

    finished navbar

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,13 +1,17 @@
-<div class="navbar">
-  <div class="w-20 h-10 rounded-full">
-    <!-- svelte-ignore a11y-missing-attribute -->
-    <p><a href="/"><img src="/logo.png" /></a></p>
+<div class="navbar bg-base-100">
+  <a href="" class="flex items-center ml-5">
+    <img src="/logo.png/" class="h-16" alt="Flowbite Logo" />
+  </a>
+
+  <div class="md:flex space-x-10 mr-20">
+    <button class="btn btn-ghost ml-16">App</button>
+    <button class="btn btn-ghost">Game</button>
   </div>
   
   <div class="navbar bg-base-100">
     <ul class="menu menu-horizontal px-10 gap-10">
-      <a href="/app" class="btn btn-ghost">APP</a>
-      <a href="/game" class="btn btn-ghost">GAME</a>
+      <button class="btn btn-ghost">App</button>
+      <button class="btn btn-ghost">Game</button>
     </ul>
   </div>
   <div>
@@ -16,3 +20,5 @@
     </ul>
   </div>
 </div>
+
+push
