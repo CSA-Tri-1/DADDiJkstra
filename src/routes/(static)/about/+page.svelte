36ba83d25252d4576ffdbef63f89196893261f03
Pitsco <derrickhuang2@gmail.com>
2023-10-02 09:29:30 -0700

    test

---
+++ src/routes/(static)/about/+page.svelte
@@ -0,0 +1,8 @@
+<div class="text-center bg-gray-1080px">
+    <div class="text-6xl tracking-wide flex justify-center items-center font-SFPro-Display-Bold">The Algorithm</div>
+    <div class="text-lg opacity-50 pt-3 flex justify-center items-center font-SFPro-Text-Regular">(di-ji-ke-shtra)</div>
+    <div class="text-lg opacity-30 pt-3 flex justify-center items-center font-SFPro-Text-Regular">pronoun</div>
+    <div class="text-lg pt-3 flex justify-center items-center font-SFPro-Text-Regular">an algorithm for finding the shortest paths between nodes in a weighted graph</div>
+</div>
+<!-- image -->
+
